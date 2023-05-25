94232b9887e2a967df7a924aeacedcb634ea18f6
Anthony Gomez <56603329+GomezAnthony@users.noreply.github.com>
2023-05-24 22:58:25 -0500

    Level 1

---
+++ Day_1/Level-1.js
@@ -0,0 +1,6 @@
+// Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+let challenge = '30 Days Of JavaScript';
+// rint the string on the browser console using console.log()
+console.log(challenge);
+// Print the length of the string on the browser console using console.log()
+console.log(challenge.length);
